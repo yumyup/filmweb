2409c8cee3b6beaf9677a1c08d00b73544fe46e2
yupi <grafik@10g.pl>
2017-06-15 17:35:42 +0200

    dodawanie opisu filmu.

--- src/main/java/com/example/demo/FIlmController.java
+++ src/main/java/com/example/demo/FIlmController.java
@@ -25,4 +25,12 @@ public class FIlmController {
         filmRepository.delete(movieId);
         return "redirect:/films";   //redirect odswierzenie strony po usunieciu filmu
     }
+
+    @GetMapping("/film")
+    public String showOpis(@RequestParam("id") Long id, Model model) {
+        Film film = filmRepository.findOne(id);
+        model.addAttribute("film", film);
+        return "film";
+    }
+
 }
